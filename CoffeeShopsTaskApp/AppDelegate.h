d01c7749f69a9c3b6b146263eb999b0b2b48e88c
Syngmaster <max6361@mail.ru>
2017-12-06 16:32:43 +0000

    Initial Commit

---
+++ CoffeeShopsTaskApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeShopsTaskApp
+//
+//  Created by Syngmaster on 06/12/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
